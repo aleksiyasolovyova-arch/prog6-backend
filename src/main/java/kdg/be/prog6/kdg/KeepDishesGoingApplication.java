c8891968b0a73ef6ec8af377fc09984c8868df7d
aleks <aleksiya.solovyova@student.kdg.be>
2025-10-03 12:02:18 +0200

    Intial commit

---
+++ src/main/java/kdg/be/prog6/kdg/KeepDishesGoingApplication.java
@@ -0,0 +1,13 @@
+package kdg.be.prog6.kdg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepDishesGoingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeepDishesGoingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kdg/be/prog6/kdg/KeepDishesGoingApplicationTests.java
@@ -0,0 +1,13 @@
+package kdg.be.prog6.kdg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepDishesGoingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
